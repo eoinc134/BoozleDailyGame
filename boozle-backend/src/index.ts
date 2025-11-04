010646559742cc4af571d6e66a1ea84e01a695a1
Eoin Costello <eoinc134@gmail.com>
2025-11-04 21:02:37 +0100

    Restructure project to move backend files into boozle-backend directory

--- boozle-backend/src/index.ts
+++ boozle-backend/src/index.ts
@@ -2,13 +2,15 @@ import express from 'express';
 import { PrismaClient } from './generated/prisma/index.js';
 import dailyCocktailRoutes from './routes/dailyCocktail.ts';
 
+// ----- Initialize App -----
 const app = express();
 const prisma = new PrismaClient()
-app.use(express.json());
 
+// ----- Middleware -----
+app.use(express.json());
 app.use('/daily-cocktail', dailyCocktailRoutes);
 
 // ----- Start Server -----
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
 });
